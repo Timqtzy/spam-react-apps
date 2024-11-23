d9980a152dae973b412681d8b4f8bc9279a13a62
Timqtzy <timothytenido@gmail.com>
2024-11-23 09:52:46 +0800

    first commit

--- spam-react-app/vite.config.js
+++ spam-react-app/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/spam-react-apps/", // Replace <REPOSITORY_NAME> with your GitHub repository name
+});
