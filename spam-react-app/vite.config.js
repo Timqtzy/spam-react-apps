f50eb61020e261ec159f3242faafadc6d6af3429
Timqtzy <timothytenido@gmail.com>
2024-12-02 15:47:46 +0800

    pls

--- spam-react-app/vite.config.js
+++ spam-react-app/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "spam-react-apps", // Replace <REPOSITORY_NAME> with your GitHub repository name
+  base: "/spam-react-apps/", // Replace <REPOSITORY_NAME> with your GitHub repository name
 });
